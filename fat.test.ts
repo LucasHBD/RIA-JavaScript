371465b89cb8aa7769790469f8bc57857e33fb26
LucasHBD <130990709+LucasHBD@users.noreply.github.com>
2024-12-06 13:29:34 +0000

    :sparkles: feat: adicionado testes negativos e melhores

--- fat.test.ts
+++ fat.test.ts
@@ -11,3 +11,15 @@ test('Teste o fatorial de 3', () => {
 test('Teste o fatorial de 10', () => {
     expect(fat(10)).toBe(3628800);
 });
+
+test('Teste o fatorial de 1', () => {
+    expect(fat(1)).toBe(1);
+});
+
+test('Teste o fatorial de 0', () => {
+    expect(fat(0)).toBe(1);
+});
+
+test('Teste o fatorial de -5', () => {
+    expect(() => fat(-5)).toThrowError('Fatorial não é definido para números negativos');
+});
--- fat.ts
+++ fat.ts
@@ -1,5 +1,8 @@
 function fat(n: number): number {
     let resultado: number = n;
+    if (n < 0) {
+        throw new Error('Fatorial não é definido para números negativos');
+    }
     if (n === 0 || n === 1) return 1;
     while (n > 1) {
         n--;
